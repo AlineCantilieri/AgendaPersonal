ce9c1a5f9f734e0a0a8b54df5fa1d67c20169264
AlineCantilieri <alinealvesmc@gmail.com>
2016-12-13 21:40:33 -0200

    Comunicação da tela Cadastros

--- app/src/main/java/br/edu/iff/pooa20162/agendapersonal/Cadastrar_Professor.java
+++ app/src/main/java/br/edu/iff/pooa20162/agendapersonal/Cadastrar_Professor.java
@@ -1,5 +1,6 @@
 package br.edu.iff.pooa20162.agendapersonal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class Cadastrar_Professor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar__professor);
+
+        Intent intent = getIntent();
+
     }
 }
--- app/src/main/java/br/edu/iff/pooa20162/agendapersonal/Cadastrar_Usuario.java
+++ app/src/main/java/br/edu/iff/pooa20162/agendapersonal/Cadastrar_Usuario.java
@@ -1,7 +1,10 @@
 package br.edu.iff.pooa20162.agendapersonal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Cadastrar_Usuario extends AppCompatActivity {
 
@@ -9,5 +12,44 @@ public class Cadastrar_Usuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar__usuario);
+
+        Intent intent = getIntent();
+
+        Button btCadastrarProfessor = (Button) findViewById(R.id.btCadastrarProfessor);
+        btCadastrarProfessor.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Cadastrar_Usuario.this, Cadastrar_Professor.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+        Button btCadastraraAcademia = (Button) findViewById(R.id.btCadastrarAcademia);
+        btCadastraraAcademia.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Cadastrar_Usuario.this, Cadastrar_Academia.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+        Button btCadastraraAluno = (Button) findViewById(R.id.btCadastrarAluno);
+        btCadastraraAluno.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Cadastrar_Usuario.this, Cadastrar_Aluno.class);
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 }
